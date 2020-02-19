b7247b2156ffdc747e9c99f1a4e6e7344863a17e
Julio Pineda <Julio Pineda@LAPTOP-SIFK8H4K>
2020-02-19 13:14:59 -0600

    j

--- conversor/Form1.Designer.cs
+++ conversor/Form1.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cboD = new System.Windows.Forms.ComboBox();
+            this.cboW = new System.Windows.Forms.ComboBox();
+            this.cboEscoger = new System.Windows.Forms.ComboBox();
+            this.txtCifra = new System.Windows.Forms.TextBox();
+            this.lblR = new System.Windows.Forms.Label();
+            this.btnConversor = new System.Windows.Forms.Button();
+            this.lblTipoConversor = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboD
+            // 
+            this.cboD.FormattingEnabled = true;
+            this.cboD.Location = new System.Drawing.Point(221, 90);
+            this.cboD.Name = "cboD";
+            this.cboD.Size = new System.Drawing.Size(121, 21);
+            this.cboD.TabIndex = 0;
+            // 
+            // cboW
+            // 
+            this.cboW.FormattingEnabled = true;
+            this.cboW.Location = new System.Drawing.Point(221, 228);
+            this.cboW.Name = "cboW";
+            this.cboW.Size = new System.Drawing.Size(121, 21);
+            this.cboW.TabIndex = 1;
+            // 
+            // cboEscoger
+            // 
+            this.cboEscoger.FormattingEnabled = true;
+            this.cboEscoger.Location = new System.Drawing.Point(12, 108);
+            this.cboEscoger.Name = "cboEscoger";
+            this.cboEscoger.Size = new System.Drawing.Size(121, 21);
+            this.cboEscoger.TabIndex = 2;
+            this.cboEscoger.SelectedIndexChanged += new System.EventHandler(this.cboTipo_SelectedIndexChanged);
+            // 
+            // txtCifra
+            // 
+            this.txtCifra.Location = new System.Drawing.Point(230, 152);
+            this.txtCifra.Name = "txtCifra";
+            this.txtCifra.Size = new System.Drawing.Size(100, 20);
+            this.txtCifra.TabIndex = 3;
+            // 
+            // lblR
+            // 
+            this.lblR.AutoSize = true;
+            this.lblR.Location = new System.Drawing.Point(55, 212);
+            this.lblR.Name = "lblR";
+            this.lblR.Size = new System.Drawing.Size(35, 13);
+            this.lblR.TabIndex = 4;
+            this.lblR.Text = "label1";
+            // 
+            // btnConversor
+            // 
+            this.btnConversor.Location = new System.Drawing.Point(32, 186);
+            this.btnConversor.Name = "btnConversor";
+            this.btnConversor.Size = new System.Drawing.Size(75, 23);
+            this.btnConversor.TabIndex = 5;
+            this.btnConversor.Text = "PUSH";
+            this.btnConversor.UseVisualStyleBackColor = true;
+            this.btnConversor.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // lblTipoConversor
+            // 
+            this.lblTipoConversor.AutoSize = true;
+            this.lblTipoConversor.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoConversor.Location = new System.Drawing.Point(69, 18);
+            this.lblTipoConversor.Name = "lblTipoConversor";
+            this.lblTipoConversor.Size = new System.Drawing.Size(211, 33);
+            this.lblTipoConversor.TabIndex = 6;
+            this.lblTipoConversor.Text = "CONVERSOR";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "CONVERTIR ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "CONVERTIR A ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "TIPOS DE CONVERSOR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(374, 290);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTipoConversor);
+            this.Controls.Add(this.btnConversor);
+            this.Controls.Add(this.lblR);
+            this.Controls.Add(this.txtCifra);
+            this.Controls.Add(this.cboEscoger);
+            this.Controls.Add(this.cboW);
+            this.Controls.Add(this.cboD);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboD;
+        private System.Windows.Forms.ComboBox cboW;
+        private System.Windows.Forms.ComboBox cboEscoger;
+        private System.Windows.Forms.TextBox txtCifra;
+        private System.Windows.Forms.Label lblR;
+        private System.Windows.Forms.Button btnConversor;
+        private System.Windows.Forms.Label lblTipoConversor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- conversor/Form1.cs
+++ conversor/Form1.cs
@@ -12,9 +12,54 @@ namespace conversor
 {
     public partial class Form1 : Form
     {
+        Conversiones objconversiones = new Conversiones();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboEscoger.Items.AddRange(objconversiones.Tipo);
+
+            cboEscoger.SelectedIndex = 0;
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+             lblR.Text = "Valor: " + objconversiones.convertir(cboD.SelectedIndex, cboW.SelectedIndex, double.Parse(txtCifra.Text), cboEscoger.SelectedIndex) + " " + objconversiones.etiquetas[cboEscoger.SelectedIndex][cboW.SelectedIndex];
+            }
+            catch 
+            {
+             MessageBox.Show("Error en la introduccion de Datos", "Conversores", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cboTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cboD.Items.Clear();
+
+            cboW.Items.Clear();
+
+            cboD.Items.AddRange(objconversiones.etiquetas[cboEscoger.SelectedIndex]);
+
+            cboW.Items.AddRange(objconversiones.etiquetas[cboEscoger.SelectedIndex]);
+
+            cboD.SelectedIndex = 0;
+
+            cboW.SelectedIndex = 1;
+
+            lblR.Text = "?";
+
+            txtCifra.Text = "1";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
+    
